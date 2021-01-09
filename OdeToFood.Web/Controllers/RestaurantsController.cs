7729fd434148ae24443adf39227877b03d2cf40c
Jay Rohrssen <jrrohrssen@hotmail.com>
2021-01-09 12:45:13 -0500

    link to db

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,9 +1,5 @@
 ﻿using OdeToFood.Data.Models;
 using OdeToFood.Data.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OdeToFood.Web.Controllers
@@ -37,17 +33,59 @@ namespace OdeToFood.Web.Controllers
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]        
         public ActionResult Create(Restaurant restaurant)
-        {
+        {            
+            if (ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
             return View();
         }
 
         public ActionResult Edit(int id)
         {
             var model = db.Get(id);
+            if(model == null)
+            {
+                return View("NotFound");
+            }
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }            
+            return View();            
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var model = db.Get(id);
+            if(model == null)
+            {
+                return RedirectToAction("NotFound");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Delete(id);
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
